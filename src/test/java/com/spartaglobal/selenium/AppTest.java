bb68b9d8963f07b2a780479a8f77d7add0ce1502
Samuel Jegede <SJegede@spartaglobal.com>
2019-05-01 14:47:21 +0100

    test for selenium

--- src/test/java/com/spartaglobal/selenium/AppTest.java
+++ src/test/java/com/spartaglobal/selenium/AppTest.java
@@ -2,19 +2,39 @@ package com.spartaglobal.selenium;
 
 import static org.junit.Assert.assertTrue;
 
+import com.spartaglobal.selenium.bbcwebsite.BBCWebsite;
+import com.spartaglobal.selenium.bbcwebsite.selenium_config.SeleniumConfig;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    private static BBCWebsite website;
+
+    @BeforeClass
+    public static void setUp(){
+        SeleniumConfig selCon = new SeleniumConfig("chrome");
+        WebDriver driver = selCon.getDriver();
+        website = new BBCWebsite(driver);
+
     }
+
+    @Test
+    public void testValidUsernameInput(){
+        website.homePage().goToHomePage().clickSignInLink();
+        website.LoginPage().inputUsername("dsajkfdkjajaf@gmail.com").inputPassword("bahdgjasfkfksjh").signIn();
+
+        Assert.assertEquals("Please include something that isn't a letter.",
+                website.LoginPage().findElement(By.xpath("//*[@id=\"form-message-password\"]/p/span/span[3]")).getText());
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        website.quitDriver();
+    }
+
 }
